1f9897dcef5139baba8dc3367a42153fdf293259
Blaer <zhangyueyang0022@126.com>
2024-08-26 21:17:39 +0300

    FIX: Action Invoke

--- Aalto-Water-Game/Assets/_Script/Tiles/TileInteraction.cs
+++ Aalto-Water-Game/Assets/_Script/Tiles/TileInteraction.cs
@@ -9,12 +9,12 @@ public class TileInteraction : MonoBehaviour
     private void OnMouseEnter()
     {
         if (EventSystem.current.IsPointerOverGameObject()) return;
-        CursorManager.OnMouseHoverOnTile(transform.position, MapManager.Instance.BuildingIsPossibleOnTile(transform.position), true);
+        CursorManager.OnMouseHoverOnTile?.Invoke(transform.position, MapManager.Instance.BuildingIsPossibleOnTile(transform.position), true);
     }
 
     private void OnMouseExit()
     {
-        CursorManager.OnMouseHoverOnTile(new Vector2(100, 100), true, false);
+        CursorManager.OnMouseHoverOnTile?.Invoke(new Vector2(100, 100), true, false);
     }
 
     private void OnMouseUpAsButton()
