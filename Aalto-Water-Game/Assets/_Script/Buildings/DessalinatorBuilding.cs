27d89b87dc89c875a6d706913ddd00933fbebc44
Blaer <zhangyueyang0022@126.com>
2024-08-28 11:08:50 +0300

    FIX: static check buildable

--- Aalto-Water-Game/Assets/_Script/Buildings/DessalinatorBuilding.cs
+++ Aalto-Water-Game/Assets/_Script/Buildings/DessalinatorBuilding.cs
@@ -7,7 +7,7 @@ public class DessalinatorBuilding : Building
     public DessalinatorBuilding(Vector2Int tilePosition, GameObject buildingSprite, GameObject dirtTileSprite) 
         : base(BuildingType.Dessalinator, tilePosition, buildingSprite, dirtTileSprite) {}
 
-    public bool CanBuildAccordingToRules(List<Tile> surroundingTiles)
+    public static bool CanBuildAccordingToRules(List<Tile> surroundingTiles)
     {
         var tileTypesCount = CountTypes(surroundingTiles);
         return tileTypesCount[TileType.SaltyWater] > 0;
